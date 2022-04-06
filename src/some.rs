fbaa1d0c6d417a4976ac03ff81aed3b858c36f31
ishibashi-futos <ishibashi.futos@outlook.com>
2022-04-06 23:15:43 +0900

    Someをテストする関数に機能を追加

--- src/some.rs
+++ src/some.rs
@@ -3,27 +3,43 @@ use std::fmt::Debug;
 
 impl<'a, T> Assert<'a, Option<T>>
 where
-    T: Debug,
+    T: Debug + PartialEq,
 {
     pub fn is_some(&self) -> &Self {
         assert!(
             self.actual.is_some(),
-            "should be equal to Some: {:?}",
+            "should be equal Some: {:?}",
             self.actual
         );
 
         self
     }
+
+    pub fn is_some_equal_to(&self, expected: &T) -> &Self {
+        assert_eq!(
+            self.is_some().actual.as_ref().unwrap(),
+            expected,
+            "should be equal Some({:?}) and {:?}",
+            expected,
+            self.actual
+        );
+
+        self
+    }
+
+    pub fn unwrap_some(&self) -> Assert<'a, T> {
+        Assert { actual: self.is_some().actual.as_ref().unwrap() }
+    }
 }
 
 impl<'a, T> Not<'a, Option<T>>
 where
-    T: Debug,
+    T: Debug + PartialEq,
 {
     pub fn is_some(&self) -> &Self {
         assert!(
             self.actual.is_none(),
-            "should be equal to None: {:?}",
+            "should be equal None: {:?}",
             self.actual
         );
 
--- tests/some_test.rs
+++ tests/some_test.rs
@@ -29,3 +29,48 @@ fn not_none() {
 
     expect(&v).not().is_some();
 }
+
+#[test]
+fn is_some_equal_to_equal() {
+    let v = Some(100);
+
+    expect(&v).is_some_equal_to(&100);
+}
+
+#[test]
+#[should_panic(expected = "should be equal Some(")]
+fn is_some_equal_to_not_equal() {
+    let v = Some(100);
+
+    expect(&v).is_some_equal_to(&99);
+}
+
+#[test]
+#[should_panic(expected = "should be equal Some:")]
+fn is_some_equal_to_none_should_panic() {
+    let v: Option<i32> = None;
+
+    expect(&v).is_some_equal_to(&99);
+}
+
+#[test]
+fn unwrap_some_has_some_str() {
+    let v = Some("Test");
+
+    expect(&v).unwrap_some().equals(&"Test");
+}
+
+#[test]
+fn unwrap_some_has_some_num() {
+    let v = Some(150);
+
+    expect(&v).unwrap_some().in_range(100..200);
+}
+
+#[test]
+#[should_panic(expected = "should be equal Some: None")]
+fn unwrap_some_none() {
+    let v: Option<i32> = None;
+
+    expect(&v).unwrap_some().in_range(100..200);
+}
